6eaaf1977e4739e8567070ee5449663704417d79
adolotova <ad4019809@gmail.com>
2022-04-30 15:21:29 +0300

    Second part was declared

---
+++ part2/Decimal.h
@@ -0,0 +1,12 @@
+#ifndef CLASSES_AND_INHERITANCE_DECIMAL_H
+#define CLASSES_AND_INHERITANCE_DECIMAL_H
+
+//Задание 46
+
+class Decimal {
+private:
+    unsigned char [100];
+};
+
+
+#endif //CLASSES_AND_INHERITANCE_DECIMAL_H
