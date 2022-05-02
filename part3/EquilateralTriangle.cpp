57bdabc5d1ad397a3bb9c36c88f68e7066b487d0
adolotova <ad4019809@gmail.com>
2022-05-02 18:24:36 +0300

    Class EquilateralTriangle

---
+++ part3/EquilateralTriangle.cpp
@@ -0,0 +1,24 @@
+#include "EquilateralTriangle.h"
+
+EquilateralTriangle::EquilateralTriangle(double p_side) :
+        Triangle(p_side, p_side, 60.0) {}
+
+double EquilateralTriangle::area() {
+    return pow(m_first_side, 2) * sqrt(3) / 4;
+}
+
+double EquilateralTriangle::perimeter() {
+    return m_first_side * 3;
+}
+
+void EquilateralTriangle::get() {
+    std::cout << "Input side: ";
+    std::cin >> m_first_side;
+    m_second_side = m_first_side;
+    m_angle = 60.0;
+    std::cout << std::endl;
+}
+
+void EquilateralTriangle::put() {
+    std::cout << "Side: " << m_first_side << std::endl;
+}
---
+++ part3/EquilateralTriangle.h
@@ -0,0 +1,22 @@
+#ifndef CLASSES_AND_INHERITANCE_EQUILATERALTRIANGLE_H
+#define CLASSES_AND_INHERITANCE_EQUILATERALTRIANGLE_H
+
+#include "Triangle.h"
+
+class EquilateralTriangle : Triangle {
+public:
+    EquilateralTriangle() = default;
+
+    EquilateralTriangle(double p_side);
+
+    double area() override;
+
+    double perimeter() override;
+
+    void get() override;
+
+    void put() override;
+};
+
+
+#endif //CLASSES_AND_INHERITANCE_EQUILATERALTRIANGLE_H
