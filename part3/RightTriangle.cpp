4fd881b3f6be574e64061d7a8dcd7e70c8d82eed
adolotova <ad4019809@gmail.com>
2022-05-02 18:09:27 +0300

    Class RightTriangle

---
+++ part3/RightTriangle.cpp
@@ -0,0 +1,27 @@
+#include "RightTriangle.h"
+
+RightTriangle::RightTriangle(double p_first_side, double p_second_side, double p_angle) :
+        Triangle(p_first_side, p_second_side, p_angle) {}
+
+double RightTriangle::area() {
+    return m_first_side * m_second_side * sin(m_angle) / 2;
+}
+
+double RightTriangle::perimeter() {
+    return m_first_side + m_second_side +
+           sqrt(pow(m_first_side, 2) + pow(m_second_side, 2));
+}
+
+void RightTriangle::get() {
+    std::cout << "Input first leg: ";
+    std::cin >> m_first_side;
+    std::cout << "Input second leg: ";
+    std::cin >> m_second_side;
+    std::cout << std::endl;
+}
+
+void RightTriangle::put() {
+    std::cout << "First leg: " << m_first_side << std::endl;
+    std::cout << "Second leg: " << m_second_side << std::endl;
+    std::cout << "Angle: " << m_angle << std::endl;
+}
---
+++ part3/RightTriangle.h
@@ -0,0 +1,23 @@
+#ifndef CLASSES_AND_INHERITANCE_RIGHTTRIANGLE_H
+#define CLASSES_AND_INHERITANCE_RIGHTTRIANGLE_H
+
+#include "Triangle.h"
+
+class RightTriangle : Triangle {
+public:
+    RightTriangle() = default;
+
+    RightTriangle(double p_first_side, double p_second_side, double p_angle);
+
+    double area() override;
+
+    double perimeter() override;
+
+    void get() override;
+
+    void put() override;
+
+};
+
+
+#endif //CLASSES_AND_INHERITANCE_RIGHTTRIANGLE_H
